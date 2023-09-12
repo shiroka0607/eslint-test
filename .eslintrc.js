c9f0f841e9ecea1f35373a096c9941bd12cb9c8e
shiroka <shiroka@eviry.com>
2023-09-12 20:32:33 +0900

    ESLint, Pretteirの設定

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'next/core-web-vitals',
+    'airbnb',
+    'airbnb-typescript',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    // 関数コンポーネントにアロー関数を使用する
+    'react/function-component-definition': [
+      2,
+      { namedComponents: 'arrow-function' },
+    ],
+    // 関数の返り値に型定義を必要とする
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      //importするファイルをjsだけではなく、tsを含むファイルを許可する
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
